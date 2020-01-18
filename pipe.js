231c4dfd3d0c2a9d3bfab12081b4019bdc1509eb
root <root@localhost.localdomain>
2020-01-18 22:36:32 +0530

    Html Pipe

--- pipe.js
+++ pipe.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function(req, res){
   console.log('request was made:' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
   myReadStream.pipe(res);
 });
 server.listen(3000);
