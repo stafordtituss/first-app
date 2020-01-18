098f7d7ea966eec5b1f76b6dbcefaa3da728b0e6
root <root@localhost.localdomain>
2020-01-18 22:27:18 +0530

    Pipes

---
+++ pipe.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(req, res){
+  console.log('request was made:' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+  myReadStream.pipe(res);
+});
+server.listen(3000);
+console.log('Listening to port 3000');
