1f4ee5d4f8eca116ca019af678c5d09debe39e4b
root <root@localhost.localdomain>
2020-01-18 22:58:30 +0530

    Routes HTML

--- http.js
+++ http.js
@@ -1,14 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.url == '/') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World');
-    res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html').pipe(res);
   }
 
-  if (req.url === '/api/courses') {
+  else if (req.url === '/users') {
+    var arr = [{name: 'Staford Titus S', age: 21}, {name: 'Sanford John', age: 18}]
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify([1, 2, 3]));
+    res.end(JSON.stringify(arr));
+
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/404.html').pipe(res);
   }
 });
 
